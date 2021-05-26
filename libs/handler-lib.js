f7bf922bb72d58e8c3ac1c677112aad31378eea7
Shehan Prasanna <shehanp068@gmail.com>
2021-05-26 22:46:42 +0530

    crud operations

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,20 +1,18 @@
 export default function handler(lambda) {
-    return async function (event, context) {
-      let body, statusCode;
-  
-      try {
-        // Run the Lambda
-        body = await lambda(event, context);
-        statusCode = 200;
-      } catch (e) {
-        body = { error: e.message };
-        statusCode = 500;
-      }
-  
-      // Return HTTP response
-      return {
-        statusCode,
-        body: JSON.stringify(body),
-      };
+  return async function (event, context) {
+    let body, statusCode;
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
     };
-  }
+  };
+}
